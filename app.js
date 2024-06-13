47c11038eb510d4e104d0cbfd988f12e4c8c0c5d
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-12 22:19:41 -0700

    add keyup listener to move down player

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 function isTouching(a, b) {
-	const aRect = a.getBoundingClientRect();
-	const bRect = b.getBoundingClientRect();
+  const aRect = a.getBoundingClientRect();
+  const bRect = b.getBoundingClientRect();
 
-	return !(
-		aRect.top + aRect.height < bRect.top ||
-		aRect.top > bRect.top + bRect.height ||
-		aRect.left + aRect.width < bRect.left ||
-		aRect.left > bRect.left + bRect.width
-	);
+  return !(
+    aRect.top + aRect.height < bRect.top ||
+    aRect.top > bRect.top + bRect.height ||
+    aRect.left + aRect.width < bRect.left ||
+    aRect.left > bRect.left + bRect.width
+  );
 }
+
+const player = document.querySelector("#player");
+window.addEventListener("keyup", function (e) {
+  const curTop = extractPos(player.style.top);
+  player.style.top = `${curTop + 50}px`;
+});
+
+const extractPos = (pos) => {
+  if (!pos) return 0;
+  return parseInt(pos.slice(0, -2));
+};
